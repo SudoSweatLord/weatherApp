f3c65e2c9e8f121c555cdbcc7de6f505f07c1898
K-Fahrenberger <fahrenberger.k@web.de>
2025-01-08 16:31:19 +0100

    fix:typo

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let appId = "660e3f7f5c0056405ff4525716659fc8";
-let units = "imperials";
-let searchMethod;
+let units = "imperial";
+let searchMethod = "zip";
 
-function searchWeather(seatchTerm) {
+function searchWeather(searchTerm) {
   fetch(
     `http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`
   )
@@ -17,3 +17,8 @@ function searchWeather(seatchTerm) {
 function init(resultFromServer) {
   console.log(resultFromServer);
 }
+
+document.getElementById("searchBtn").addEventListener("click", () => {
+  let searchTerm = document.getElementById("searchInput").value;
+  if (searchTerm) searchWeather(searchTerm);
+});
