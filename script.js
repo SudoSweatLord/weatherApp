0397d89c0334786d4325f0d3ff395d6e7b0ff775
K-Fahrenberger <fahrenberger.k@web.de>
2025-01-08 16:50:04 +0100

    differentiate between zip code or spelled area

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 let appId = "660e3f7f5c0056405ff4525716659fc8";
 let units = "imperial";
-let searchMethod = "zip";
+let searchMethod;
+
+function getSearchMethod(searchTerm) {
+  if (
+    searchTerm.length === 5 &&
+    Number.parseInt(searchTerm) + "" === searchTerm
+  )
+    searchMethod = "zip";
+  else searchMethod = "q";
+}
 
 function searchWeather(searchTerm) {
+  getSearchMethod(searchTerm);
   fetch(
     `http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`
   )
