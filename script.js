44794f2748caf482e965d34ef6d7703d6d29461d
K-Fahrenberger <fahrenberger.k@web.de>
2025-01-08 17:36:14 +0100

    add: variables for future use; icons are visible

--- script.js
+++ script.js
@@ -25,7 +25,45 @@ function searchWeather(searchTerm) {
 }
 
 function init(resultFromServer) {
-  console.log(resultFromServer);
+  switch (resultFromServer.weather[0].main) {
+    case "Clear":
+      document.body.style.backgroundImage = "url('./images/clear.jpg')";
+      break;
+    case "Clouds":
+      document.body.style.backgroundImage = "url('./images/cloud.jpg')";
+      break;
+    case "Rain":
+      document.body.style.backgroundImage = "url('./images/rain.jpg')";
+      break;
+    case "Drizzle":
+      document.body.style.backgroundImage = "url('./images/drizzle.jpg')";
+
+      break;
+    case "Mist":
+      document.body.style.backgroundImage = "url('./images/mist.jpg')";
+
+      break;
+    case "Thunderstorm":
+      document.body.style.backgroundImage = "url('./images/thunderstorm.jpg')";
+
+      break;
+    case "Snow":
+      document.body.style.backgroundImage = "url('./images/snow.jpg')";
+
+      break;
+  }
+  let weatherDescriptionHeader = document.getElementById(
+    "weatherDescriptionHeader"
+  );
+  let temperatureElement = document.getElementById("temperature");
+  let humidityElement = document.getElementById("humidity");
+  let windSpeedElement = document.getElementById("windSpeed");
+  let cityHeader = document.getElementById("cityHeader");
+  let weatherIcon = document.getElementById("documentIconImg");
+  weatherIcon.src =
+    "https://openweathermap.org/img/w/" +
+    resultFromServer.weather[0].icon +
+    ".png";
 }
 
 document.getElementById("searchBtn").addEventListener("click", () => {
