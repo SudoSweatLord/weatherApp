66777104060d41499de7d5213143b2162e02e2b6
K-Fahrenberger <fahrenberger.k@web.de>
2025-01-21 12:38:58 +0100

    fix: background image does not repeat; fix: text does not wrap

--- script.js
+++ script.js
@@ -79,11 +79,9 @@ function init(resultFromServer) {
 }
 function setPositionForWeatherInfo() {
   let weatherContainer = document.getElementById("weatherContainer");
-  let weatherContainerHeight = weatherContainer.clientHeight;
-  let weatherContainerWidth = weatherContainer.clientWidth;
+  // let weatherContainerHeight = weatherContainer.clientHeight;
+  // let weatherContainerWidth = weatherContainer.clientWidth;
 
-  weatherContainer.style.left = `calc(50% - ${weatherContainerWidth / 2}px)`;
-  weatherContainer.style.top = `calc(50% - ${weatherContainerHeight / 2}px)`;
 
   weatherContainer.style.visibility = "visible";
 }
